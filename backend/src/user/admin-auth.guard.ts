981a7642c1f57c79f03fa03099a516518c9f21bd
zulja1 <luka.zuljo@gmail.com>
2025-03-31 19:08:06 +0200

    Add role based user authorization

---
+++ backend/src/user/admin-auth.guard.ts
@@ -0,0 +1,17 @@
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Role } from '@prisma/client';
+
+@Injectable()
+export class AdminAuthGuard extends AuthGuard('jwt') {
+  async canActivate(context: ExecutionContext) {
+    const baseGuardResult = await super.canActivate(context);
+    if (!baseGuardResult) {
+      return false;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    return user.role === Role.Admin;
+  }
+}
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaService } from 'src/prisma.service';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, PrismaService],
+  providers: [UserService, PrismaService, JwtStrategy],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
 })
 export class UserModule {}
